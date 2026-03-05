ad4cd99effa93a110c2316f3b013c09ef87c3f44
Abhi <dasxabhi04@gmail.com>
2026-03-06 00:21:13 +0530

    Delete travel_planner directory

--- travel_planner/src/routes/tripRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const tripController = require('../controllers/tripController');
-
-router.post('/create', tripController.createTrip);
-router.get('/:id', tripController.getTripById); // This is the dynamic route
-
-module.exports = router;
--- travel_planner/src/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-
-// Save preferences (Veg/Non-Veg, Currency, etc.)
-router.put('/update-profile', userController.updatePreferences);
-
-module.exports = router;
